161215a3f605bdf85adfcef5794c8b242b7aff3b
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-10-19 15:15:41 +0100

    Update Folha5.hs

--- Ex/Folha5.hs
+++ Ex/Folha5.hs
@@ -35,3 +35,56 @@ zip' xs ys= zipWith1 f xs ys
   where f a b = (a,b)
 
 --5
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f []= []
+takeWhile' f (x:xs) = if f x then (x:takeWhile' f xs) else []
+
+--6
+dropWhile' :: (a -> Bool)-> [a] -> [a]
+dropWhile' f []= []
+dropWhile' f  (x:xs) = if f x then dropWhile f xs else x:xs
+
+--7
+dropUntil' :: (a -> Bool)-> [a] -> [a]
+dropUntil' f []= []
+dropUntil' f (x:xs) = if f x then x:xs else dropUntil' f xs
+
+--8
+total1 :: (Int -> Int)-> Int -> Int
+total1 f x = totalAux1 f x 0
+
+totalAux1 :: (Int -> Int)-> Int -> Int -> Int
+totalAux1 f x n = if x == n then f x else f n + totalAux1 f x (n+1)
+
+total2 :: (Int -> Int)-> Int -> Int
+total2 f x = sum [f y | y <- [0..x]]
+
+total3 :: (Int -> Int)-> Int -> Int
+total3 f x = sum (map f [0..x])
+
+--9
+aplica1 :: [a -> a] -> [a] -> [a]
+aplica1 [] xs = xs
+aplica1 (f:fs) xs = aplica1 fs (map f xs)
+
+aplica2 :: [a -> a] -> [a] -> [a]
+aplica2 fs xs = foldr (\f acc -> map f acc) xs (reverse fs)
+
+aplica3 :: [a -> a] -> [a] -> [a]
+aplica3 fs xs = foldl (\acc f -> map f acc) xs fs
+
+--10
+---a Num a => a -> a
+---b 7
+---c (Ord a, Num a) => a -> Bool
+---d Num a => a -> a -> a
+---e Error
+---f 10
+---g Num a => a -> a -> a
+---h (a -> a) -> a -> a
+---i devolve Num
+
+--11
+----(\a b c -> a*b*c)1 2 
+
+--12
