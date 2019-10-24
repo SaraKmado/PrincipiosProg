9005df0d14589c17af0d157e2687e581185307ce
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-10-24 20:38:39 +0100

    A done

--- TPC/t3_fc52806.hs
+++ TPC/t3_fc52806.hs
@@ -6,3 +6,5 @@ distancia (x:xs) = fst (foldl (\(acc,y) p -> (acc + dist y p,p)) (0,x) xs)
 
 dist :: (Float,Float) -> (Float,Float) -> Float
 dist (x1,y1) (x2,y2) = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+
+--B
