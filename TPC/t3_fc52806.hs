ee28d5489e09e502957873044191bee0b5321b7b
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-10-27 10:46:16 +0000

    B e C feitos: terminado

--- TPC/t3_fc52806.hs
+++ TPC/t3_fc52806.hs
@@ -1,5 +1,8 @@
 --Sara Queimado 52806
 
+--para usar: fold, map, filter
+--usado: fold, map, filter
+
 --A
 distancia :: [(Float,Float)] -> Float
 distancia (x:xs) = fst (foldl (\(acc,y) p -> (acc + dist y p,p)) (0,x) xs)
@@ -8,3 +11,13 @@ dist :: (Float,Float) -> (Float,Float) -> Float
 dist (x1,y1) (x2,y2) = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
 --B
+minimaDistanciaA :: (Float,Float) -> [(Float,Float)] -> Float
+minimaDistanciaA x xs = minimum (map (dist x) xs)
+
+--C
+evitaPontos :: Float -> [(Float,Float)] -> [(Float,Float)] -> [(Float,Float)]
+evitaPontos d [x] ys = filter (\y -> (dist x y) > d) ys
+evitaPontos d (x:xs) ys = (exists (evitaPontos d xs ys) (filter (\y -> (dist x y) > d) ys))
+
+exists :: [(Float,Float)] -> [(Float,Float)] -> [(Float,Float)]
+exists xs ys = [x | x <- xs, elem x ys]
