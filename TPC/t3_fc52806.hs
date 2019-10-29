74fafb4b927fd84ccd8f98b12ecd1ec439c5a9eb
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-10-29 15:06:52 +0000

    Update t3_fc52806.hs

--- TPC/t3_fc52806.hs
+++ TPC/t3_fc52806.hs
@@ -5,17 +5,23 @@
 
 --A
 distancia :: [(Float,Float)] -> Float
+distancia [] = 0
 distancia (x:xs) = fst (foldl (\(acc,y) p -> (acc + dist y p,p)) (0,x) xs)
 
 dist :: (Float,Float) -> (Float,Float) -> Float
 dist (x1,y1) (x2,y2) = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
 --B
-minimaDistanciaA :: (Float,Float) -> [(Float,Float)] -> Float
+minimaDistanciaA :: (Float,Float) -> [(Float,Float)] -> Float 
+minimaDistanciaA _ [] = error a lista e vazia
 minimaDistanciaA x xs = minimum (map (dist x) xs)
 
 --C
 evitaPontos :: Float -> [(Float,Float)] -> [(Float,Float)] -> [(Float,Float)]
+evitaPontos d [] xs = xs
+
+evitaPontos d _ [] = []
+
 evitaPontos d [x] ys = filter (\y -> (dist x y) > d) ys
 evitaPontos d (x:xs) ys = (exists (evitaPontos d xs ys) (filter (\y -> (dist x y) > d) ys))
 
