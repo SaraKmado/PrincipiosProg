e8ed1364aeb5699d7a11890711d7d189e30826d9
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-11-04 14:27:43 +0000

    Update t3_fc52806.hs

--- TPC/t3_fc52806.hs
+++ TPC/t3_fc52806.hs
@@ -6,15 +6,15 @@
 --A
 distancia :: [(Float,Float)] -> Float
 distancia [] = 0
-distancia (x:xs) = fst (foldl (\(acc,y) p -> (acc + dist y p,p)) (0,x) xs)
+distancia (x:xs) = fst $ foldl (\(acc,y) p -> (acc + dist y p,p)) (0,x) xs
 
 dist :: (Float,Float) -> (Float,Float) -> Float
 dist (x1,y1) (x2,y2) = sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
 --B
 minimaDistanciaA :: (Float,Float) -> [(Float,Float)] -> Float 
-minimaDistanciaA _ [] = error "a lista e vazia"
-minimaDistanciaA x xs = minimum (map (dist x) xs)
+minimaDistanciaA _ [] = error "Empty list"
+minimaDistanciaA x xs = minimum $ map (dist x) xs
 
 --C
 evitaPontos :: Float -> [(Float,Float)] -> [(Float,Float)] -> [(Float,Float)]
@@ -24,4 +24,4 @@ evitaPontos d [x] ys = filter (\y -> (dist x y) > d) ys
 evitaPontos d (x:xs) ys = (exists (evitaPontos d xs ys) (filter (\y -> (dist x y) > d) ys))
 
 exists :: [(Float,Float)] -> [(Float,Float)] -> [(Float,Float)]
-exists xs ys = [x | x <- xs, elem x ys]
+exists xs ys = filter (`elem` ys) xs
