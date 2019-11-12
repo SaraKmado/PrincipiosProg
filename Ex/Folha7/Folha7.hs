28995b71206e74d1044bea6ce8d3f5c380e67819
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-11-12 16:14:51 +0000

    Update Folha7.hs

--- Ex/Folha7/Folha7.hs
+++ Ex/Folha7/Folha7.hs
@@ -1,1 +1,19 @@
---s
+
+--1
+writePrimes :: [Int] -> IO()
+writePrimes xs = do
+  let t = generateTo (1 + maximum xs) 2 []
+  let text = foldl (\acc x -> acc ++ (show x) ++ "th prime is " ++ (show $ t !! x) ++ "\n") "" xs
+  print text
+
+generateTo :: Int -> Int -> [Int]-> [Int]
+generateTo maxi curr list = if maxi == length list
+  then list
+  else if prime curr
+    then generateTo maxi (curr + 1) (list ++ [curr])
+    else generateTo maxi (curr + 1) list
+
+prime :: Int -> Bool
+prime x = foldl (\acc y -> if mod x y == 0 then False else acc) True [2..(x-1)]
+
+--2
