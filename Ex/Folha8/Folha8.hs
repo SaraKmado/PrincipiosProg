1cd28c8aee0e532e2b53af345e1a17b61f17c182
SaraKmado <56612999+SaraKmado@users.noreply.github.com>
2019-11-13 11:32:25 +0000

    Update Folha8.hs

--- Ex/Folha8/Folha8.hs
+++ Ex/Folha8/Folha8.hs
@@ -1,3 +1,4 @@
+import Control.Monad
 --1
 writePrimes :: [Int] -> IO()
 writePrimes xs = do
@@ -16,3 +17,40 @@ prime :: Int -> Bool
 prime x = foldl (\acc y -> if mod x y == 0 then False else acc) True [2..(x-1)]
 
 --2
+palindrome :: String -> Bool
+palindrome xs = xs == reverse xs
+---a
+isP1 = do
+  line <- getLine
+  print (if palindrome line then "Sim" else "Nao")
+---b
+isP2 = do
+  line <- getLine
+  when (not $ null line) $ do
+    print (if palindrome line then "Sim" else "Nao")
+    isP2
+---c
+isP3 = interact palindromeWrite
+
+palindromeWrite allLines = unlines $ foldr (\x acc -> if palindrome x then "Sim" : acc else "Nao" : acc) [] (lines allLines)
+
+--3
+---a
+printEven :: Int -> IO()
+printEven x = if even x then print "Par" else print "Impar"
+
+---b
+showParity1 :: [Int] -> IO()
+showParity1 [] = error "Empty list"
+showParity1 [x] = printEven x
+showParity1 (x:xs) = do
+  printEven x
+  showParity1 xs
+
+---c
+showParity2 :: [Int] -> IO()
+showParity2 xs = mapM_ printEven xs
+
+--4
+----ler um numero n. depois ler n inteiros e soma los
+----ver somar.hs
